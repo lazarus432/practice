704dfe0f7032e537bd40e816592008d26ed5eb12
Duex <deux02030@gmail.com>
2017-11-30 11:10:24 -0500

    anonymous, splice, and foreach

--- practice.js
+++ practice.js
@@ -16,4 +16,35 @@ function buildTriangle(num) {
     return triangle;
 }
 
-console.log(buildTriangle(15));
+// anonymous function that takes a number as input and outputs the number of ha's
+var laugh = function(num){
+    lau = "";
+    for(var x=0; x<num; x++){
+        lau +="ha";
+    }
+    return lau + '!';
+}
+
+console.log(laugh(10));
+
+// use splice to remove blackberry and add yellow, green, and purple
+var rainbow = ["Red", "Orange", "Blackberry", "Blue"];
+
+rainbow.splice(2,1, "Yellow", "Green");
+rainbow.splice(5,1, "Purple");
+
+console.log(rainbow);
+
+// write forEach loop that adds 100 to each number divisible by 3
+var test = [12, 929, 11, 3, 199, 1000, 7, 1, 24, 37, 4,
+    19, 300, 3775, 299, 36, 209, 148, 169, 299,
+    6, 109, 20, 58, 139, 59, 3, 1, 139
+];
+
+test.forEach(function(element,index){
+    if(element % 3 === 0){
+        element += 100;
+        test.splice(index, 1, element);
+    }
+})
+console.log(test);
