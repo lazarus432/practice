e2cd26220e97d8db034c09afabe6e378c370f560
Duex <deux02030@gmail.com>
2017-12-19 15:47:25 -0500

    docs: working with objects
    3 object related problem examples.

--- practice.js
+++ practice.js
@@ -70,3 +70,65 @@ for(row=0; row < numbers.length; row++){
         console.log(numbers[row][col]);
     }
 }
+
+// Add account summary to return account message
+
+var savingsAccount = {
+    balance: 1000,
+    interestRatePercent: 1,
+    deposit: function addMoney(amount) {
+        if (amount > 0) {
+            savingsAccount.balance += amount;
+        }
+    },
+    withdraw: function removeMoney(amount) {
+        var verifyBalance = savingsAccount.balance - amount;
+        if (amount > 0 && verifyBalance >= 0) {
+            savingsAccount.balance -= amount;
+        }
+    },
+    printAccountSummary: function summary() {
+        var summary = "Welcome!\nYour balance is currently $" + savingsAccount.balance + " and your interest rate is " + savingsAccount.interestRatePercent +"%.";
+        return summary;
+    }
+};
+
+console.log(savingsAccount.printAccountSummary());
+
+// Create an object with three properties. The object should also have 4 methods. 
+
+facebookProfile = {
+    name: "Nic",
+    friends: 300,
+    messages: ["hello"],
+    postMessage: function postMessage(message) {
+        return messages = facebookProfile.messages.push(message);
+    },
+    deleteMessage: function deleteMessage(index) {
+        return remove = facebookProfile.messages.splice(index, 1);
+    },
+    addFriend: function addFriend() {
+        return facebookProfile.friends += 1;
+    },
+    removeFriend: function removeFriend() {
+        return facebookProfile.friends -= 1;
+    }
+};
+
+console.log(facebookProfile.postMessage("hiya"));
+console.log(facebookProfile.messages);
+console.log(facebookProfile.deleteMessage(0));
+console.log(facebookProfile.messages);
+
+// Use forEach to loop over array and print out donut summaries
+
+var donuts = [
+    { type: "Jelly", cost: 1.22 },
+    { type: "Chocolate", cost: 2.45 },
+    { type: "Cider", cost: 1.59 },
+    { type: "Boston Cream", cost: 5.99 }
+];
+
+donuts.forEach(function(donut) {
+    console.log(donut.type + " donuts cost $" + donut.cost + " each");
+});
